a330240ec94edde474e7b20e95ac0ada25b8f467
hafeezkhannsr <hafeezkhannsr@users.noreply.github.com>
2025-10-18 00:13:16 +0500

    Cleanup: Remove unused files and folders

--- server/models/Admin.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const AdminSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Admin', AdminSchema);
--- server/models/Attendance.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const AttendanceSchema = new mongoose.Schema({
-  studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
-  courseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
-  date: { type: Date, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Attendance', AttendanceSchema);
